e138eef0cda9d84cba943bcae0fca3464d4cc721
hun2__2 <wognskec@hanyang.ac.kr>
2023-01-08 21:09:10 +0900

    01.08 위장 풀이(py,js)

--- programmers/01.hash/04.위장/js/01.js
+++ programmers/01.hash/04.위장/js/01.js
@@ -1,4 +1,22 @@
 function solution(clothes) {
-    var answer = 0;
-    return answer;
+    let map = {};
+    clothes.map((item) => (map[item[1]] ? map[item[1]]++ : (map[item[1]] = 1)));
+
+    return Object.values(map).reduce((total, now) => total * (now + 1), 1) - 1;
 }
+
+//test
+console.log(
+    solution([
+        ["yellow_hat", "headgear"],
+        ["blue_sunglasses", "eyewear"],
+        ["green_turban", "headgear"],
+    ])
+);
+// console.log(
+//     solution([
+//         ["crow_mask", "face"],
+//         ["blue_sunglasses", "face"],
+//         ["smoky_makeup", "face"],
+//     ])
+// );
--- programmers/01.hash/04.위장/python/01.py
+++ programmers/01.hash/04.위장/python/01.py
@@ -1,3 +1,28 @@
-def solution(clothes):
-    answer = 0
-    return answer
+def solution(args):
+    
+    map ={}
+    for arg in args:
+        map[arg[1]] = map[arg[1]] + 1 if arg[1] in map else 1
+            
+    answer = 1
+    for arg in map.values():
+        answer *= arg + 1
+    return answer - 1
+
+
+
+print(
+    solution([
+        ["yellow_hat", "headgear"],
+        ["blue_sunglasses", "eyewear"],
+        ["green_turban", "headgear"],
+    ])
+);
+
+print(
+    solution([
+        ["crow_mask", "face"],
+        ["blue_sunglasses", "face"],
+        ["smoky_makeup", "face"],
+    ])
+);
