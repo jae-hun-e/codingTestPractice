50a0da9153a0236be5419f739097c1969b74709f
hun2__2 <wognskec@hanyang.ac.kr>
2023-01-09 19:09:54 +0900

    01.09 전화번호 목록 효율성 실패..

--- programmers/01.hash/03.전화번호 목록/python/01.py
+++ programmers/01.hash/03.전화번호 목록/python/01.py
@@ -1,18 +1,16 @@
 #  효율성 실패 
 def solution(phone_book):
     phone_book.sort(key=len)
-
-    for i in range(len(phone_book)):
-        for num in phone_book[i+1:]:
-            if num[:len(phone_book[i])] == phone_book[i]:
-                print(num[:len(phone_book[i])])
-                print(phone_book[i])
+    for i, num1 in enumerate(phone_book):
+        for num2 in (phone_book[i+1:]):
+            if num1 == num2[:len(num1)]:  
                 return False
+        
     return True
 
 
 
 # test
-# print(solution([ "97674223", "1195524421","119"]))
-# print(solution(["123","456","789"]))
-# print(solution(["12","123","1235","567","88"]))
+print(solution([ "97674223", "1195524421","119"]))
+print(solution(["123","456","789"]))
+print(solution(["12","123","1235","567","88"]))
