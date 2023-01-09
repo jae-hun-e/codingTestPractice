a654ea41311f2507a84178990d7d4ad00cbd8a31
hun2__2 <wognskec@hanyang.ac.kr>
2023-01-09 22:00:57 +0900

    01.09 전화번호 목록 효율성 성공!

--- programmers/01.hash/03.전화번호 목록/python/01.py
+++ programmers/01.hash/03.전화번호 목록/python/01.py
@@ -1,11 +1,11 @@
 #  효율성 실패 
 def solution(phone_book):
-    phone_book.sort(key=len)
-    for i, num1 in enumerate(phone_book):
-        for num2 in (phone_book[i+1:]):
-            if num1 == num2[:len(num1)]:  
-                return False
-        
+    phone_book.sort()
+
+    for i, prev in enumerate(phone_book[:-1]):
+        if prev == phone_book[i+1][:len(prev)]:
+            return False
+    
     return True
 
 
