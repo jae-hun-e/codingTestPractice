d63012326b205e034ab95695f21e40b9a8c6f99e
hun2__2 <wognskec@hanyang.ac.kr>
2023-01-10 22:16:07 +0900

    01.10 베스트 앨범

---
+++ programmers/01.hash/05.베스트앨범/js/01.js
@@ -0,0 +1,15 @@
+/*
+많은 장르 먼저 
+    장르 내  많이 재생
+        고유번호 낮은 노래
+
+
+*/
+
+function solution(genres, plays) {
+    var answer = [];
+    return answer;
+}
+
+// test
+solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]);
