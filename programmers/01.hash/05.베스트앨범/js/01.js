ed07ad123bf866eab242d477e4e7ae1f7a005bd9
hun2__2 <wognskec@hanyang.ac.kr>
2023-01-11 22:28:41 +0900

    01.11 js코드 품 매우 마음에 안듬

--- programmers/01.hash/05.베스트앨범/js/01.js
+++ programmers/01.hash/05.베스트앨범/js/01.js
@@ -6,10 +6,35 @@
 
 */
 
+/*
+map = {
+    장르 : [[재생횟수, 번호]]
+}
+*/
+
 function solution(genres, plays) {
-    var answer = [];
+    let answer = [];
+    const mapping = genres.reduce((map, now, i) => {
+        const arr = map[now] ? map[now] : [0];
+        arr[0] += plays[i];
+        arr.push([plays[i], i]);
+        map[now] = arr;
+        return map;
+    }, {});
+
+    const arr = Object.keys(mapping).map((genres) => {
+        mapping[genres].sort((a, b) => b[0] - a[0]);
+        return [genres, mapping[genres][0]];
+    });
+    arr.sort((a, b) => b[1] - a[1]);
+
+    arr.forEach((genres) =>
+        mapping[genres[0]].slice(1, 3).map((sing) => answer.push(sing[1]))
+    );
     return answer;
 }
 
 // test
-solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]);
+console.log(
+    solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500])
+);
