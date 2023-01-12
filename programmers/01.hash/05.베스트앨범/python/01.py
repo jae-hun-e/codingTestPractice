79ae61da370210e5dd87a2a933504ab69964d831
hun2__2 <wognskec@hanyang.ac.kr>
2023-01-12 17:39:20 +0900

    01.11 py코드 왜 틀리는 거지??

--- programmers/01.hash/05.베스트앨범/python/01.py
+++ programmers/01.hash/05.베스트앨범/python/01.py
@@ -1,3 +1,21 @@
+# 왜틀리는거지??
 def solution(genres, plays):
     answer = []
+
+    mapping = {}
+    for i, (genre, play) in enumerate(zip(genres, plays)):
+        arr = mapping[genre] if genre in mapping else [0]
+        arr[0] += play
+        arr += [[play, i]]
+        mapping[genre] = arr
+
+    for (genre, list) in sorted(mapping.items(), key= lambda x : x[0], reverse=True):
+        for (play, idx) in sorted(list[1:], key= lambda x : x[0], reverse=True)[:2]:
+            answer.append(idx)
+    
     return answer
+
+
+
+#test
+print(solution(["classic", "pop", "classic", "classic", "pop", "test"],[500, 600, 150, 800, 2500, 8000]))
