e19445281107334fefe8606d9519e03b584d4858
hun2__2 <wognskec@hanyang.ac.kr>
2023-04-20 09:28:50 +0900

    그지같은 백준문제...으으

--- fastcampus/part1/ch04.그리디/baekjoon/11509.js
+++ fastcampus/part1/ch04.그리디/baekjoon/11509.js
@@ -1,27 +1,44 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().split("\n");
-// const input = ["5", "2 1 5 4 3"];
-const num = Number(input[0]);
+// const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const input = ["5", "2 1 5 4 3"];
 let arr = input[1].split(" ").map(Number);
 
+// sol2 - 메모리 터짐
 let cnt = 0;
-let n = arr.length;
-for (let i = 0; i < num; i++) {
-    if (arr.length === 0) break;
-
-    // 하나 쏨
-    const a = arr.shift();
+while (arr.length) {
+    // console.log(arr);
+    let tmp = arr.shift();
     cnt++;
+    tmp--;
 
-    shot(a, n);
+    while (1) {
+        if (arr.includes(tmp)) {
+            arr = arr.filter((v) => v !== tmp);
+            tmp--;
+        } else break;
+    }
 }
 console.log(cnt);
 
-function shot(a, n) {
-    const tmp = arr.filter((v) => v !== a - 1);
-    if (tmp.length !== arr.length) {
-        n--;
-        arr = tmp;
-        shot(a - 1, n);
-    }
-}
+// // sol1 - call stack error
+// let cnt = 0;
+// let n = arr.length;
+// for (let i = 0; i < num; i++) {
+//     if (arr.length === 0) break;
+
+//     // 하나 쏨
+//     const a = arr.shift();
+//     cnt++;
+
+//     shot(a, n);
+// }
+// console.log(cnt);
+
+// function shot(a, n) {
+//     const tmp = arr.filter((v) => v !== a - 1);
+//     if (tmp.length !== arr.length) {
+//         n--;
+//         arr = tmp;
+//         shot(a - 1, n);
+//     }
+// }
