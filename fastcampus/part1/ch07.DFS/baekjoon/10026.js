cf4959a55c2944923f143a82d534d03d7a9388cb
hun2__2 <wognskec@hanyang.ac.kr>
2023-05-12 20:42:59 +0900

    dfs... 내일 다시 풀지..

---
+++ fastcampus/part1/ch07.DFS/baekjoon/10026.js
@@ -0,0 +1,7 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const [n, k] = input[0].split(" ").map(Number);
+
+for (let i = 1; i <= n; i++) {
+    const [g, x] = input[i].split(" ").map(Number);
+}
