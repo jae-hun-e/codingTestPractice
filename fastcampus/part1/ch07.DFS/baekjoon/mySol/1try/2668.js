841316201c7766623ac052eae9bb4ac943fab2ba
hun2__2 <wognskec@hanyang.ac.kr>
2023-07-11 00:27:59 +0900

    [dfs] 순서도를 이용해서 문제풀어보기 연습, DFS중 방향이있는 그래프의 사이클 탐색 문제2 (느낌이 온다..!!)

--- fastcampus/part1/ch07.DFS/baekjoon/mySol/1try/2668.js
+++ fastcampus/part1/ch07.DFS/baekjoon/mySol/1try/2668.js
@@ -1,2 +1,43 @@
-const input = ["7", "3", "1", "1", "5", "5", "4", "6"];
-// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+// const input = ["7", "3", "1", "1", "5", "5", "4", "6"];
+
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const n = Number(input[0]);
+
+const graph = [0];
+for (let i = 1; i <= n; i++) {
+    graph.push(Number(input[i]));
+}
+// console.log("graph", graph);
+const visited = new Array(n + 1).fill(false);
+const finished = new Array(n + 1).fill(false);
+const selected = [];
+
+let print = "";
+
+for (let i = 1; i <= n; i++) {
+    if (!visited[i]) dfs(i);
+}
+
+function dfs(cur) {
+    visited[cur] = true;
+
+    let next = graph[cur];
+
+    if (!visited[next]) {
+        dfs(next);
+    } else if (!finished[next]) {
+        while (next !== cur) {
+            selected.push(next);
+            next = graph[next];
+        }
+        selected.push(next);
+    }
+
+    finished[cur] = true;
+}
+
+print += selected.length + "\n";
+selected.sort((a, b) => a - b).forEach((v) => (print += v + "\n"));
+
+console.log(print);
--- fastcampus/part1/ch07.DFS/baekjoon/mySol/2try/9466(순서도연습).js
+++ fastcampus/part1/ch07.DFS/baekjoon/mySol/2try/9466(순서도연습).js
@@ -1,5 +1,5 @@
 // const input = ["2", "7", "3 1 3 7 3 4 6", "8", "1 2 3 4 5 6 7 8"]; // 3,0
-const input = ["3", "5", "2 3 4 5 4", "4", "2 3 4 2", "3", "2 3 3"]; // 3,1,2
+// const input = ["3", "5", "2 3 4 5 4", "4", "2 3 4 2", "3", "2 3 3"]; // 3,1,2
 
 const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
