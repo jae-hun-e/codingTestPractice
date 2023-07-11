9722f46a2d2e8bbd2ff648643bd783fd6356eff3
hun2__2 <wognskec@hanyang.ac.kr>
2023-07-11 14:41:45 +0900

    [dfs] 할렐루야다ㅜㅜㅜ 감이 조금씩 오고있어ㅜㅜㅜ끄앙ㅠㅠ

--- fastcampus/part1/ch07.DFS/baekjoon/mySol/1try/10026.js
+++ fastcampus/part1/ch07.DFS/baekjoon/mySol/1try/10026.js
@@ -1,63 +1,61 @@
-// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-
 const input = ["5", "RRRBB", "GGBBB", "BBBRR", "BBRRR", "RRRRR"];
 
-/**
- * R, G, B 3가지 색이 있다.
- * 같은색상 상하좌우 인접한 경우 같은 구역
- * 적록색약 -> R, G 같은 것으로 봄
- *
- * sol
- * dfs로 상하좌우 탐색하며 방문 기록더 이상 없으면
- */
-const n = Number(input[0]);
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const arr = input.slice(1);
-const visited = Array.from({ length: arr.length }, () => Array(n).fill(false));
-// console.log(visited);
+const N = Number(input[0]);
 
-const arr2 = arr.map((line) =>
-    line.split("").map((item) => {
-        return item === "R" ? "G" : item;
-    })
-);
-const visited2 = Array.from({ length: arr.length }, () => Array(n).fill(false));
+const graph = [],
+    graph2 = [];
+const visited = Array.from({ length: N }, () => new Array(N).fill(false));
+const visited2 = Array.from({ length: N }, () => new Array(N).fill(false));
+let cnt1 = 0;
+cnt2 = 0;
+
+for (let i = 1; i <= N; i++) {
+    const arr = input[i].split("");
+    graph.push(arr);
+
+    const arr2 = arr.map((v) => {
+        if (v === "R") return "G";
+        else return v;
+    });
+    graph2.push(arr2);
+}
+
+// console.log(graph, graph2);
 
 const dx = [-1, 1, 0, 0],
     dy = [0, 0, -1, 1];
 
-let cnt1 = 0,
-    cnt2 = 0;
+for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+        if (!visited[i][j]) {
+            cnt1++;
+            // console.log("cnt : ", cnt);
+            // console.log(visited);
+            dfs(i, j, graph, visited);
+        }
 
-function dfs(y, x, color, arr, visit) {
-    for (let i = 0; i < 4; i++) {
-        const nx = x + dx[i];
-        const ny = y + dy[i];
-        // console.log("i", i, "y,x", y, x, "nx:", nx, "ny:", ny);
-        if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
-        if (color === arr[ny][nx] && !visit[ny][nx]) {
-            visit[ny][nx] = true;
-            // console.log(visit);
-            dfs(ny, nx, color, arr, visit);
+        if (!visited2[i][j]) {
+            cnt2++;
+            dfs(i, j, graph2, visited2);
         }
     }
 }
 
-// arr[y][x]
-// i -> y
-for (let i = 0; i < n; i++) {
-    //  j -> x
-    for (let j = 0; j < n; j++) {
-        if (!visited[i][j]) {
-            visited[i][j] = true;
-            dfs(i, j, arr[i][j], arr, visited);
-            cnt1++;
-        }
-        if (!visited2[i][j]) {
-            visited2[i][j] = true;
-            dfs(i, j, arr2[i][j], arr2, visited2);
-            cnt2++;
-        }
+function dfs(y, x, graph, visited) {
+    visited[y][x] = true;
+
+    // console.log("y, x", y, x);
+    const cur = graph[y][x];
+    for (let i = 0; i < 4; i++) {
+        const nx = x + dx[i],
+            ny = y + dy[i];
+
+        if (ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
+
+        // console.log("ny, nx", ny, nx);
+        if (!visited[ny][nx] && cur === graph[ny][nx]) dfs(ny, nx, graph, visited);
     }
 }
 
--- fastcampus/part1/ch07.DFS/baekjoon/mySol/1try/14502.js
+++ fastcampus/part1/ch07.DFS/baekjoon/mySol/1try/14502.js
@@ -1,1 +1,87 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+// const input = [
+//     "7 7",
+//     "2 0 0 0 1 1 0",
+//     "0 0 1 0 1 2 0",
+//     "0 1 1 0 1 0 0",
+//     "0 1 0 0 0 0 0",
+//     "0 0 0 0 0 1 1",
+//     "0 1 0 0 0 0 0",
+//     "0 1 0 0 0 0 0",
+// ];
+
+const input = ["4 6", "0 0 0 0 0 0", "1 0 0 0 0 2", "1 1 1 0 0 2", "0 0 0 0 0 2"];
+
+// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const [n, m] = input[0].split(" ").map(Number);
+
+const g = [];
+const zero = [];
+
+for (let i = 1; i <= n; i++) {
+    const line = input[i].split(" ").map(Number);
+    g.push(line);
+    line.forEach((v, j) => {
+        if (v === 0) zero.push([i - 1, j]);
+    });
+}
+
+const zeroVisited = new Array(zero.length).fill(false);
+// console.log(g, zero);
+let ans = 0;
+// const selected = [];
+
+const selected = [];
+function dfs1(dep, idx) {
+    if (dep === 3) {
+        // console.log("selected", zero[selected[0]], zero[selected[1]], zero[selected[2]]);
+        const visited = Array.from({ length: n }, () => new Array(m).fill(false));
+        const result = g.map((v) => [...v]);
+        selected.forEach((i) => (result[zero[i][0]][zero[i][1]] = 1));
+        // console.log("result1", result);
+
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < m; j++) {
+                if (!visited[i][j] && g[i][j] === 2) dfs2(i, j, result, visited);
+            }
+        }
+
+        let sum = 0;
+        result.forEach((line) => (sum += line.filter((v) => v === 0).length));
+        // console.log("sum", sum);
+        ans = Math.max(ans, sum);
+        return;
+    }
+
+    for (let i = idx; i < zeroVisited.length; i++) {
+        if (!zeroVisited[i]) {
+            zeroVisited[i] = true;
+            selected.push(i);
+            dfs1(dep + 1, idx + 1);
+            selected.pop();
+            zeroVisited[i] = false;
+        }
+    }
+}
+
+const dx = [-1, 1, 0, 0],
+    dy = [0, 0, -1, 1];
+
+function dfs2(y, x, graph, visited) {
+    // console.log("graph", y, x, graph);
+    visited[y][x] = true;
+
+    for (let i = 0; i < 4; i++) {
+        const nx = x + dx[i];
+        const ny = y + dy[i];
+
+        if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+        if (!visited[ny][nx] && graph[ny][nx] === 0) {
+            graph[ny][nx] = 2;
+            dfs2(ny, nx, graph, visited);
+        }
+    }
+}
+
+dfs1(0, 0);
+console.log(ans);
