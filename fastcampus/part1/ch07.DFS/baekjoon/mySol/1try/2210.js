53a66679fbb8d811fb8530dbc310c647bcaf0941
hun2__2 <wognskec@hanyang.ac.kr>
2023-07-15 01:28:17 +0900

    폴더 정리, 2210 코드 수정

--- fastcampus/part1/ch07.DFS/baekjoon/mySol/1try/2210.js
+++ fastcampus/part1/ch07.DFS/baekjoon/mySol/1try/2210.js
@@ -5,7 +5,6 @@ const graph = [];
 for (let i = 0; i < 5; i++) graph.push(input[i].split(" "));
 
 // console.log(graph);
-const visited = Array.from({ length: 5 }, () => new Array(5).fill(false));
 
 const dy = [-1, 1, 0, 0],
     dx = [0, 0, -1, 1];
@@ -20,8 +19,6 @@ function dfs(y, x, dep) {
         return;
     }
 
-    visited[y][x] = true;
-
     // console.log("cur", cur);
 
     for (let i = 0; i < 4; i++) {
@@ -30,7 +27,7 @@ function dfs(y, x, dep) {
 
         if (ny < 0 || ny >= 5 || nx < 0 || nx >= 5) continue;
 
-        // if (!visited[ny][nx])
+
         cur += graph[ny][nx];
         // console.log("cur1", cur);
         dfs(ny, nx, dep + 1);
@@ -41,7 +38,6 @@ function dfs(y, x, dep) {
 
 for (let i = 0; i < 5; i++) {
     for (let j = 0; j < 5; j++) {
-        // if (!visited[i][j])
         cur += graph[i][j];
         dfs(i, j, 1);
         cur = cur.slice(0, cur.length - 1);
