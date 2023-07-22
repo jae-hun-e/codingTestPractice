7bab30665d352ae78fe84a3fe8d2cd97efae2886
hun2__2 <wognskec@hanyang.ac.kr>
2023-07-22 23:28:38 +0900

    [bfs] class로 구현과 array의 차이

--- fastcampus/part1/ch08.BFS/baekjoin/mySol/1try/24444.js
+++ fastcampus/part1/ch08.BFS/baekjoin/mySol/1try/24444.js
@@ -2,6 +2,23 @@ const input = ["5 5 1", "1 4", "1 2", "2 3", "2 4", "3 4"];
 // const input = ["6 4 1", "2 3", "1 4", "1 5", "4 6"];
 // const input = require("fs").readFileSync("dev/stdin").toString().trim().split("\n");
 
+class Que {
+    q = [];
+    h = 0;
+    t = 0;
+    enque(v) {
+        this.q[this.t++] = v;
+    }
+    deque() {
+        const v = this.q[this.h];
+        delete this.q[this.h++];
+        return v;
+    }
+    size() {
+        return this.t - this.h;
+    }
+}
+
 const [n, m, r] = input[0].split(" ").map(Number);
 const graph = Array.from({ length: n + 1 }, () => []);
 for (let i = 1; i <= m; i++) {
@@ -13,17 +30,17 @@ graph.forEach((line) => line.sort((a, b) => a - b));
 // console.log(graph);
 const visited = new Array(n + 1).fill(0);
 
-const queue = [];
-queue.push(r);
+const queue = new Que();
+queue.enque(r);
 visited[r] = 1;
 
 let step = 1;
-while (queue.length) {
-    const cur = queue.shift();
+while (queue.size()) {
+    const cur = queue.deque();
 
     for (const next of graph[cur]) {
         if (!visited[next]) {
-            queue.push(next);
+            queue.enque(next);
             visited[next] = ++step;
         }
     }
